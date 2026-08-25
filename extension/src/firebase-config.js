a0cf45125d7ddb336667bf73c2fc9ec424bafceb
drgigy <drgigy@gmail.com>
2026-08-25 11:04:17 +0530

    Use Nurse WOYZ Firebase for transfer extension

--- extension/src/firebase-config.js
+++ extension/src/firebase-config.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyCuFA7wegr5nl-pdZaE3XbazuMZIb-xPLY",
-  authDomain: "stroke-code-demo-1.firebaseapp.com",
-  projectId: "stroke-code-demo-1",
-  storageBucket: "stroke-code-demo-1.firebasestorage.app",
-  messagingSenderId: "1098364915585",
-  appId: "1:1098364915585:web:c182ddd7ac23c38fada280"
+  apiKey: "AIzaSyDWKcS-5qLVnZ3OiZTtfq_kHC3aVDo6i1o",
+  authDomain: "nurse-woyz-demo.firebaseapp.com",
+  projectId: "nurse-woyz-demo",
+  storageBucket: "nurse-woyz-demo.firebasestorage.app",
+  messagingSenderId: "638967435304",
+  appId: "1:638967435304:web:36df353a4d4b9cb8d4e51d"
 };
